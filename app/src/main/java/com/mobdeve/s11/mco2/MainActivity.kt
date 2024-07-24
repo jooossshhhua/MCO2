06150220d0486adbab344f542b205e3edd49a1a3
izzywincy <izabella_imperial@dlsu.edu.ph>
2024-07-25 03:39:33 +0800

    Fix dateTextView

--- app/src/main/java/com/mobdeve/s11/mco2/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/mco2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mobdeve.s11.mco2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import java.text.SimpleDateFormat
+import java.util.Locale
 import com.mobdeve.s11.mco2.ui.theme.MCO2_S11Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +23,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val dateTextView = findViewById<TextView>(R.id.dateTextView)
+        val currentDate = SimpleDateFormat("MMMM d, yyyy", Locale.getDefault()).format(System.currentTimeMillis())
+        dateTextView.text = currentDate
 
         val spendingButton = findViewById<Button>(R.id.spendingFormBtn)
         spendingButton.setOnClickListener{
